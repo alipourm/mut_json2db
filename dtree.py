f5b2b90156d3e7c82449e3e2a9322d62078d9432
Amin Alipour <aminalipour@gmail.com>
2015-01-09 16:37:24 -0800

    scratch-- incomplete

--- dtree.py
+++ dtree.py
@@ -17,36 +17,49 @@ def standardize(A):
     return (A - np.mean(A)) / np.std(A)
     
 
+def allcombinations(featurset):
+    raise NotImplemented
 
 
-def learn_dtree(data):
+
+def learn_dtree(data, image_prefix):
     clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=4)
-    data['ope'] = map(lambda s: s.replace('org.pitest.mutationtest.engine.gregor.mutators.', ''), data['mutator'])
-    data['operator'] = map(lambda s: re.sub(r'experimental.RemoveSwitchMutator.*', 'experimental.RemoveSwitchMutator', s), data['ope'])
-    print len(data), len(data['operator'])
-   # k = data.groupby(['operator', 'class', 'method'])
-    k = data.groupby(['operator'])
-    f= k['isCovered'].agg({'mean': np.mean, 'len': len, 'sum':np.sum})
+    
+  #  print len(data), len(data['operator'])
+    data['mf'] = data['class'] + ' ' + data['method']
+    k = data.groupby(['mf'])
+
+#    k = data.groupby(['operator'])
+    f = k['isCovered'].agg({'mean': np.mean, 'len': len, 'sum':np.sum})
+    ff = lambda x: x.line
+    gg = lambda y: y-np.min(y)
+    f
+    g = k['line'].agg({'max': np.max, 'min':np.min})
+    print 'g:', g
     fig = plt.figure()
 
-#    ax = Axes3D(fig)
-   # ax = fig.add_subplot(111, projection='3d')
+    # ax = Axes3D(fig)
+    # ax = fig.add_subplot(111, projection='3d')
 
-    plt.scatter( f['mean'], (f['len']))
-#    ax.set_xlabel('mean')
-#    ax.set_ylabel('len')
-#    ax.set_zlabel('sum')
+    plt.scatter(standardize (f['mean']),f['len'])
+    plt.ylabel('mutant_size')
+    plt.xlabel('expected_kill (standatdize)')
+#    print f[f['len'] > 25000] 
+    # ax.set_xlabel('mean')
+    # ax.set_ylabel('len')
+    # ax.set_zlabel('sum')
+    
     plt.show()
-#    plt.show()
-    #for m in k.groups:
-    #    print m,len(k.groups[m]),
+
+    # plt.show()
+    # for m in k.groups:
+    #   print m,len(k.groups[m]),
     data['op'] = pd.factorize(data['operator'])[0]
     data['m'] = pd.factorize(data['method'])[0]
     data['c'] = pd.factorize(data['class'])[0]
-    plt.hist(data['op'])
-    plt.hist(data['op'])
-#    plt.show()
-#    plt.close()
+
+    # plt.show()
+    # plt.close()
     x = data[['op', 'c', 'testId']].values
     y = data['isCovered'].values
     clf.fit(x,y)
@@ -55,7 +68,7 @@ def learn_dtree(data):
     return dot_data.getvalue()
     # plt.Image(graph.create_png())
 
-
+#raw_data['norm_pos'] = raw_data.groupby(['cm','operator']).transform(lambda x: np.sum(x[x['line'] > 0]))
 
 def main(file_name):
     db_file = file_name
@@ -69,14 +82,16 @@ def main(file_name):
     print mut_coverage.columns
     mut_coverage.columns = [u'testId', u'mutantId', u'isCovered']
     print mut_coverage.columns
-    k = pd.merge(mutants, mut_coverage)
-    return k
+    data = pd.merge(mutants, mut_coverage)
+    data['ope'] = map(lambda s: s.replace('org.pitest.mutationtest.engine.gregor.mutators.', ''), data['mutator'])
+    data['operator'] = map(lambda s: re.sub(r'experimental.RemoveSwitchMutator.*', 'experimental.RemoveSwitchMutator', s), data['ope'])
+    return data
 
 
-db1 = 'data/com_lang.db'
-k = main(sys.argv[1])
-dot = learn_dtree(k)
-open(sys.argv[2], 'w').write(dot)
+#db1 = 'data/com_lang.db'
+#k = main(sys.argv[1])
+#dot = learn_dtree(k, sys.argv[2])
+# open(sys.argv[3], 'w').write(dot)
 
 
 
