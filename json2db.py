c9cac19c9ca05cdbdc2b15b446bdab9f632ecd10
Amin Alipour <amin.alipour@gmail.com>
2015-01-18 20:27:59 -0800

    .

--- json2db.py
+++ json2db.py
@@ -79,10 +79,10 @@ def do_stuff(f):
     parts = j.split('.')
     db_name = ''.join(parts[:-1]) + '.sqlite'
     if db_name + '.db' not in glob.glob(dir_name + "*.db"):
-    try:
-	do(db_name, j)	
-    except TypeError:
-	print "Couldn't  finish {0}".format(db_name)
+    	try:
+		do(db_name, j)	
+    	except TypeError:
+		print "Couldn't  finish {0}".format(db_name)
 
 
 
